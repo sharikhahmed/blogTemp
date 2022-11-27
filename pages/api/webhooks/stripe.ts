113eefd2df5c0c613c05b2ed2659b2ba15dd86cd
Sharikh Ahmed <sharikhahmed30@gmail.com>
2022-11-27 14:42:55 +0530

    [FEAT] comment out everything related to stripe

--- pages/api/webhooks/stripe.ts
+++ pages/api/webhooks/stripe.ts
@@ -19,63 +19,63 @@ export default async function handler(
   const body = await rawBody(req)
   const signature = req.headers["stripe-signature"]
 
-  let event: Stripe.Event
+  // let event: Stripe.Event
 
-  try {
-    event = stripe.webhooks.constructEvent(
-      body,
-      signature,
-      process.env.STRIPE_WEBHOOK_SECRET
-    )
-  } catch (error) {
-    return res.status(400).send(`Webhook Error: ${error.message}`)
-  }
+  // try {
+  //   event = stripe.webhooks.constructEvent(
+  //     body,
+  //     signature,
+  //     process.env.STRIPE_WEBHOOK_SECRET
+  //   )
+  // } catch (error) {
+  //   return res.status(400).send(`Webhook Error: ${error.message}`)
+  // }
 
-  const session = event.data.object as Stripe.Checkout.Session
+  // const session = event.data.object as Stripe.Checkout.Session
 
-  if (event.type === "checkout.session.completed") {
-    // Retrieve the subscription details from Stripe.
-    const subscription = await stripe.subscriptions.retrieve(
-      session.subscription as string
-    )
+  // if (event.type === "checkout.session.completed") {
+  //   // Retrieve the subscription details from Stripe.
+  //   const subscription = await stripe.subscriptions.retrieve(
+  //     session.subscription as string
+  //   )
 
-    // Update the user stripe into in our database.
-    // Since this is the initial subscription, we need to update
-    // the subscription id and customer id.
-    await db.user.update({
-      where: {
-        id: session.metadata.userId,
-      },
-      data: {
-        stripeSubscriptionId: subscription.id,
-        stripeCustomerId: subscription.customer as string,
-        stripePriceId: subscription.items.data[0].price.id,
-        stripeCurrentPeriodEnd: new Date(
-          subscription.current_period_end * 1000
-        ),
-      },
-    })
-  }
+  //   // Update the user stripe into in our database.
+  //   // Since this is the initial subscription, we need to update
+  //   // the subscription id and customer id.
+  //   await db.user.update({
+  //     where: {
+  //       id: session.metadata.userId,
+  //     },
+  //     data: {
+  //       stripeSubscriptionId: subscription.id,
+  //       stripeCustomerId: subscription.customer as string,
+  //       stripePriceId: subscription.items.data[0].price.id,
+  //       stripeCurrentPeriodEnd: new Date(
+  //         subscription.current_period_end * 1000
+  //       ),
+  //     },
+  //   })
+  // }
 
-  if (event.type === "invoice.payment_succeeded") {
-    // Retrieve the subscription details from Stripe.
-    const subscription = await stripe.subscriptions.retrieve(
-      session.subscription as string
-    )
+  // if (event.type === "invoice.payment_succeeded") {
+  //   // Retrieve the subscription details from Stripe.
+  //   const subscription = await stripe.subscriptions.retrieve(
+  //     session.subscription as string
+  //   )
 
-    // Update the price id and set the new period end.
-    await db.user.update({
-      where: {
-        stripeSubscriptionId: subscription.id,
-      },
-      data: {
-        stripePriceId: subscription.items.data[0].price.id,
-        stripeCurrentPeriodEnd: new Date(
-          subscription.current_period_end * 1000
-        ),
-      },
-    })
-  }
+  //   // Update the price id and set the new period end.
+  //   await db.user.update({
+  //     where: {
+  //       stripeSubscriptionId: subscription.id,
+  //     },
+  //     data: {
+  //       stripePriceId: subscription.items.data[0].price.id,
+  //       stripeCurrentPeriodEnd: new Date(
+  //         subscription.current_period_end * 1000
+  //       ),
+  //     },
+  //   })
+  // }
 
   return res.json({})
 }
--- prisma/migrations/20221118173244_add_stripe_columns/migration.sql
+++ prisma/migrations/20221118173244_add_stripe_columns/migration.sql
@@ -21,7 +21,7 @@ ALTER TABLE `users` ADD COLUMN `stripe_current_period_end` DATETIME(3) NULL,
     ADD COLUMN `stripe_subscription_id` VARCHAR(191) NULL;
 
 -- CreateIndex
-CREATE UNIQUE INDEX `users_stripe_customer_id_key` ON `users`(`stripe_customer_id`);
+-- CREATE UNIQUE INDEX `users_stripe_customer_id_key` ON `users`(`stripe_customer_id`);
 
 -- CreateIndex
 CREATE UNIQUE INDEX `users_stripe_subscription_id_key` ON `users`(`stripe_subscription_id`);
