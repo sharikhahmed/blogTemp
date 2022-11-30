b2768be10dbb42e4be7341213c8e068127a32c5a
Sharikh Ahmed <sharikhahmed30@gmail.com>
2022-11-30 12:01:21 +0530

    [FEAT] remove github stars function from marketing page

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -2,37 +2,8 @@ import Link from "next/link"
 import Image from "next/image"
 
 import hero from "../../public/images/hero.png"
-import { siteConfig } from "@/config/site"
-
-async function getGitHubStars(): Promise<string | null> {
-  try {
-    const response = await fetch(
-      "https://api.github.com/repos/shadcn/taxonomy",
-      {
-        headers: {
-          Accept: "application/vnd.github+json",
-          Authorization: `Bearer ${process.env.GITHUB_ACCESS_TOKEN}`,
-        },
-        next: {
-          revalidate: 60,
-        },
-      }
-    )
-
-    if (!response?.ok) {
-      return null
-    }
-
-    const json = await response.json()
-
-    return parseInt(json["stargazers_count"]).toLocaleString()
-  } catch (error) {
-    return null
-  }
-}
 
 export default async function IndexPage() {
-  const stars = await getGitHubStars()
 
   return (
     <>
